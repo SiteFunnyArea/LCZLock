37c35abe3406c323be7fe20d161469c78e248ad7
Ruemena <rue.heart.mena@gmail.com>
2023-07-21 18:18:12 -0500

    hhh

---
+++ LCZLock/Config.cs
@@ -0,0 +1,20 @@
+﻿namespace LCZLock
+{
+    using System.ComponentModel;
+
+    using Exiled.API.Interfaces;
+
+    public sealed class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+         
+        public bool Debug { get; set; } = false;
+
+        [Description("How long until the elevators are activated")]
+        public float TimeUntilUnlock { get; set; } = 40f;
+
+        public Boolean DoCassieMessage { get; set; } = false;
+        public string CassieMessage { get; set; } = "bell_start the lockdown on the light containment zone has ended . . elevators are now active bell_end";
+        public string Subtitles { get; set; } = "The lockdown on the Light Containment Zone has ended. Elevators are now active.";
+    }
+}
---
+++ LCZLock/LCZLock.cs
@@ -0,0 +1,38 @@
+﻿anamespace LCZLock
+{
+    using Exiled.API.Enums;
+    using Exiled.API.Features;
+    using Exiled.LCZLockEvents;
+
+    public class LCZLock : Plugin<Config>
+    {
+        private RoundHandler roundHandler;
+
+        public override string Author { get; } = "Rue";
+        public override string Name { get; } = "LCZLock";
+        public override string Prefix { get; } = "LCZLock";
+        public override Version Version { get; } = new Version(1, 0, 0);
+        public override Version RequiredExiledVersion { get; } = new Version(7, 2, 0);
+
+        public override PluginPriority Priority { get; } = PluginPriority.Last;
+
+        private LCZLock() { }
+        private static readonly LCZLock Singleton = new LCZLock();
+        public static LCZLock Instance => Singleton;
+
+        public override void OnEnabled()
+        {
+            roundHandler = new RoundHandler();
+            Exiled.Events.Handlers.Server.RoundStarted += roundHandler.OnRoundStarted;
+            Exiled.Events.Handlers.Server.RoundEnded += roundHandler.OnRoundEnded;
+            base.OnEnabled();
+        }
+        public override void OnDisabled()
+        {
+            Exiled.Events.Handlers.Server.RoundStarted -= roundHandler.OnRoundStarted;
+            Exiled.Events.Handlers.Server.RoundEnded -= roundHandler.OnRoundEnded;
+            roundHandler = null;
+            base.OnDisabled();
+        }
+    }
+}
